408d1799146ae086f23dcd2595c22d6e7ef6ec35
yadaramsrikanth <yadaramsrikanth2000@gmail.com>
2025-08-18 14:12:52 +0530

    form-validation-added-for-both-forms

--- src/components/RTIActivistForm/index.js
+++ src/components/RTIActivistForm/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import  { useContext, useState } from "react";
 import DatePicker from "react-datepicker";
 import Modal from "react-modal"
 import "react-datepicker/dist/react-datepicker.css";
@@ -8,69 +8,249 @@ Modal.setAppElement("#root");
 
 
 const RTIActivistForm=()=>{
-  const {isActivistModal,setIsActivistModal}=useContext(ReporterActivistContext)
- const [dob,setDob]=useState(null)
+  const initialFormdata={
+  name:"",DOB:null,gender:"",phone:"",
+  email:"",address:"",aadhar:"",media:null,
+  designation:"",pincode:"",experience:""}
+
+   const {isActivistModal,setIsActivistModal}=useContext(ReporterActivistContext)
+ const [errors,setErrors]=useState({})
+ const [isSubmitted,setIsFormSubmitted]=useState(false)
+ const [formData,setFormData]=useState(initialFormdata)
   // const {onClose,isOpen}=props
+
+const onhandleChangeActivist=(e)=>{
+  const{name,value,type,files}=e.target
+  setFormData({...formData,
+    [name]:type==="file"?files[0]:value
+  })
+}
+
+const validate=()=>{
+  const newErrors={}
+
+  if(!formData.name) newErrors.name="Name is Required"
+  if(!formData.DOB) newErrors.DOB="Date Of Birth is Required"
+  if(!formData.gender) newErrors.gender="Please select Gender"
+  if(!formData.phone) newErrors.phone="Please Enter a Phone Number"
+  if(!formData.email) newErrors.phone="Gmail is Required"
+  if(!formData.address) newErrors.address="Address is Required"
+  
+  if(!formData.media) newErrors.media="Please Upload a Photo"
+  if(!formData.designation) newErrors.designation="Designation is Required"
+  if(!formData.pincode) newErrors.pincode="Pincode is Required"
+  if(!formData.experience) newErrors.experience="Enter 0 if you have no experience"
+  setErrors(newErrors)
+  return Object.keys(newErrors).length===0
+}
+
+
+
+
+const submiActivistForm= async (event)=>{
+   event.preventDefault()
+   if (validate()){
+       console.log("Form submitted:", formData);
+       try{
+        const formDataToSend = new FormData();
+
+    
+    Object.keys(formData).forEach((key) => {
+      if (key === "DOB") {
+        formDataToSend.append(key, formData.DOB ? formData.DOB.toISOString() : "");
+      } else {
+        formDataToSend.append(key, formData[key]);
+      }
+    });
+        const reporterUrl="http://34.100.231.173:3000/api/v1/activist/application"
+        const options={method:"POST",
+        
+        body:formDataToSend
+        }
+        const data= await fetch(reporterUrl,options)
+        const responseData=await data.json()
+        console.log(responseData)
+        setFormData(initialFormdata)
+        setErrors({})
+        setIsFormSubmitted(true)
+
+      setTimeout(() => {
+      setIsFormSubmitted(false);
+      setIsActivistModal(false)
+    }, 3000);
+       }catch(e){
+        console.error("Error Submitting form",e)
+       }
+   }else{
+      console.log("Validation errors:", errors);
+   }
+
+
+  
+  
+  
+}
+
+
     return <Modal 
     isOpen={isActivistModal}
     onRequestClose={()=>setIsActivistModal(false)}
-     contentLabel="Reporter Join Form"
+    contentLabel="Reporter Join Form"
     className="modal-content"
     overlayClassName="modal-overlay"
     >
+      {isSubmitted? <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h2>Activist Form is Submitted!</h2>
+    </div>:
       
-      
-      <form className="rti-reporter-form-container">
+      <form  encType="multipart/form-data" className="rti-reporter-form-container" onSubmit={submiActivistForm}>
         <h3 className="rti-reporter-heading">RTI ACTIVIST FORM</h3>
-
-
-
-
-
        <label className="rti-reporter-label-element">Name:</label>
-        <input className="rti-reporter-input-element" type="text"  placeholder="John Doe"/>
+        <input value={formData.name} name="name" className="rti-reporter-input-element" type="text"  placeholder="John Doe" onChange={onhandleChangeActivist}/>
+        {errors.name && <span style={{ color: "red" }}>{errors.name}</span>}
+         
          <label className="rti-reporter-label-element">Select Your DOB:</label>
           {/* <input className="rti-reporter-input-element" type="date"  placeholder="DD-MM-YYYY"/> */}
-        <DatePicker
-        id="dob"
-        selected={dob}
-        onChange={(date) => setDob(date)}
-        placeholderText="15/08/1947"
-        dateFormat="dd/MM/yyyy"
-        className="rti-reporter-input-element"
-        
-        showYearDropdown
-        scrollableYearDropdown
-      />
+               <DatePicker
+                    id="dob"
+                    selected={formData.DOB}
+                    onChange={(date) =>setFormData({...formData,DOB:date})}
+                    placeholderText="15/08/1947"
+                    dateFormat="dd/MM/yyyy"
+                    className="rti-reporter-input-element"
+                   
+                    showYearDropdown
+                    scrollableYearDropdown
+                  />
+        {errors.DOB&& <span style={{ color: "red" }}>{errors.DOB}</span>}
 
              <label className="rti-reporter-label-element">Select Your Gender:</label>
 
-    <select className="rti-reporter-input-element">
+    <select value={formData.gender}  name="gender" className="rti-reporter-input-element" placeholder="Male/Female/Other" onChange={onhandleChangeActivist}>
         <option value="">Select gender</option>
         <option value="male">Male</option>
         <option value="female">Female</option>
         <option value="other">Other</option>
   </select>
+          {errors.gender && <span style={{ color: "red" }}>{errors.gender}</span>}
+
    <label className="rti-reporter-label-element">Enter Your Phone Number:</label>
-     <input  className="rti-reporter-input-element" type="tel"  placeholder="+91 98765 43210"/>
+     <input value={formData.phone} name="phone"  className="rti-reporter-input-element" type="tel"  placeholder="+91 98765 43210" onChange={onhandleChangeActivist}/>
+      {errors.phone && <span style={{ color: "red" }}>{errors.phone}</span>}
+
              <label className="rti-reporter-label-element">Enter Your Gmail:</label>
-<input  className="rti-reporter-input-element" type="email" placeholder="example@mail.com"/>
+<input  value={formData.email} name="email" className="rti-reporter-input-element" type="email" placeholder="example@mail.com" onChange={onhandleChangeActivist}/>
+            {errors.email && <span style={{ color: "red" }}>{errors.email}</span>}
+
+    
     <label className="rti-reporter-label-element">Enter Your Address:</label>
-<input  className="rti-reporter-input-element" type="text"  placeholder="123 Main St, City" />
+<input value={formData.address} name="address"  className="rti-reporter-input-element" type="text"  placeholder="123 Main St, City"  onChange={onhandleChangeActivist}/>
+            {errors.address && <span style={{ color: "red" }}>{errors.address}</span>}
+
      <label className="rti-reporter-label-element">Enter Your Adhaar Number:</label>
-      <input  className="rti-reporter-input-element" type="text"  placeholder="1234 5678 9012"  maxLength="12"/>
+      <input value={formData.aadhar} name="aadhar"  className="rti-reporter-input-element" type="text"  placeholder="1234 5678 9012"  maxLength="12" onChange={onhandleChangeActivist}/>
+             {errors.aadhar && <span style={{ color: "red" }}>{errors.aadhar}</span>}
+
      <label className="rti-reporter-label-element">Upload Your Photo:</label>
-    <input  className="rti-reporter-input-element" type="file"  accept="image/*"/>
+    <input name="media"  className="rti-reporter-input-element" type="file"  accept="image/*" onChange={onhandleChangeActivist}/>
+            {errors.media && <span style={{ color: "red" }}>{errors.media}</span>}
+
     <label className="rti-reporter-label-element">Designation:</label>
-    <input  className="rti-reporter-input-element" type="text"  placeholder="Designation Eg..Activist" />
-     <label className="rti-reporter-label-element">Enter Your Pincode:</label>
-    <input  className="rti-reporter-input-element" type="text"  placeholder="560001" />
+    <input value={formData.designation}  name="designation" className="rti-reporter-input-element" type="text"  placeholder="Designation Eg..Activist"  onChange={onhandleChangeActivist}/>
+   
+
+             {errors.designation && <span style={{ color: "red" }}>{errors.designation}</span>}
+
+      <label className="rti-reporter-label-element">Enter Your Pincode:</label>
+    <input value={formData.pincode}  name="pincode"  className="rti-reporter-input-element" type="text"  placeholder="560001" onChange={onhandleChangeActivist}/>
+             {errors.pincode && <span style={{ color: "red" }}>{errors.pincode}</span>}
+
      <label className="rti-reporter-label-element">Experience:</label>
-    <input  className="rti-reporter-input-element" type="text"  placeholder="Previous Experience If any like 3 years" />
-<button onClick={()=>setIsActivistModal(false)} type="submit" className="rti-reporter-submit-button">JOIN AS ACTIVIST</button>
+    <input value={formData.experience}  name="experience" className="rti-reporter-input-element" type="text"  placeholder="Previous Experience If any like 3 years"  onChange={onhandleChangeActivist}/>
+         {errors.experience && <span style={{ color: "red" }}>{errors.experience}</span>}
 
-
-    </form> </Modal> 
+ <button type="submit" className="rti-reporter-submit-button">JOIN AS ACTIVIST</button>
+{/* onClick={()=>setIsReporterModal(false)} */}
+    </form> }
+    </Modal> 
 }
 
 export default RTIActivistForm
+
+// import { useContext, useState } from "react";
+// import DatePicker from "react-datepicker";
+// import Modal from "react-modal"
+// import "react-datepicker/dist/react-datepicker.css";
+// import ReporterActivistContext from "../../ReactContext";
+// import "./index.css"
+// Modal.setAppElement("#root");
+
+
+
+// const RTIActivistForm=()=>{
+//   const {isActivistModal,setIsActivistModal}=useContext(ReporterActivistContext)
+//  const [dob,setDob]=useState(null)
+//   // const {onClose,isOpen}=props
+//     return <Modal 
+//     isOpen={isActivistModal}
+//     onRequestClose={()=>setIsActivistModal(false)}
+//      contentLabel="Reporter Join Form"
+//     className="modal-content"
+//     overlayClassName="modal-overlay"
+//     >
+      
+      
+//       <form className="rti-reporter-form-container">
+//         <h3 className="rti-reporter-heading">RTI ACTIVIST FORM</h3>
+
+
+
+
+
+//        <label className="rti-reporter-label-element">Name:</label>
+//         <input className="rti-reporter-input-element" type="text"  placeholder="John Doe"/>
+//          <label className="rti-reporter-label-element">Select Your DOB:</label>
+//           {/* <input className="rti-reporter-input-element" type="date"  placeholder="DD-MM-YYYY"/> */}
+//         <DatePicker
+//         id="dob"
+//         selected={dob}
+//         onChange={(date) => setDob(date)}
+//         placeholderText="15/08/1947"
+//         dateFormat="dd/MM/yyyy"
+//         className="rti-reporter-input-element"
+        
+//         showYearDropdown
+//         scrollableYearDropdown
+//       />
+
+//              <label className="rti-reporter-label-element">Select Your Gender:</label>
+
+//     <select className="rti-reporter-input-element">
+//         <option value="">Select gender</option>
+//         <option value="male">Male</option>
+//         <option value="female">Female</option>
+//         <option value="other">Other</option>
+//   </select>
+//    <label className="rti-reporter-label-element">Enter Your Phone Number:</label>
+//      <input  className="rti-reporter-input-element" type="tel"  placeholder="+91 98765 43210"/>
+//              <label className="rti-reporter-label-element">Enter Your Gmail:</label>
+// <input  className="rti-reporter-input-element" type="email" placeholder="example@mail.com"/>
+//     <label className="rti-reporter-label-element">Enter Your Address:</label>
+// <input  className="rti-reporter-input-element" type="text"  placeholder="123 Main St, City" />
+//      <label className="rti-reporter-label-element">Enter Your Adhaar Number:</label>
+//       <input  className="rti-reporter-input-element" type="text"  placeholder="1234 5678 9012"  maxLength="12"/>
+//      <label className="rti-reporter-label-element">Upload Your Photo:</label>
+//     <input  className="rti-reporter-input-element" type="file"  accept="image/*"/>
+//     <label className="rti-reporter-label-element">Designation:</label>
+//     <input  className="rti-reporter-input-element" type="text"  placeholder="Designation Eg..Activist" />
+//      <label className="rti-reporter-label-element">Enter Your Pincode:</label>
+//     <input  className="rti-reporter-input-element" type="text"  placeholder="560001" />
+//      <label className="rti-reporter-label-element">Experience:</label>
+//     <input  className="rti-reporter-input-element" type="text"  placeholder="Previous Experience If any like 3 years" />
+// <button onClick={()=>setIsActivistModal(false)} type="submit" className="rti-reporter-submit-button">JOIN AS ACTIVIST</button>
+
+
+//     </form> </Modal> 
+// }
+
+// export default RTIActivistForm
--- src/components/RTIReporterForm/index.js
+++ src/components/RTIReporterForm/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import  { useContext, useState } from "react";
 import DatePicker from "react-datepicker";
 import Modal from "react-modal"
 import "react-datepicker/dist/react-datepicker.css";
@@ -8,23 +8,87 @@ Modal.setAppElement("#root");
 
 
 const RTIReporterForm=()=>{
-  const {isReporterModal,setIsReporterModal}=useContext(ReporterActivistContext)
- const [reporterdob,setReporterDob]=useState(null)
- const [formData,setFormData]=useState({name:"",})
+  const initialFormdata={
+  name:"",DOB:null,gender:"",phone:"",
+  email:"",address:"",aadhar:"",media:null,
+  designation:"",pincode:"",experience:""}
+
+   const {isReporterModal,setIsReporterModal}=useContext(ReporterActivistContext)
+ const [errors,setErrors]=useState({})
+ const [isSubmitted,setIsFormSubmitted]=useState(false)
+ const [formData,setFormData]=useState(initialFormdata)
   // const {onClose,isOpen}=props
-const submitReporterForm=async (event)=>{
-  event.preventDefault()
-  const reporterUrl="http://34.100.231.173:3000/api/v1/reporter/application"
-  const options={method:"POST",
-     headers: {
-    'Content-Type': 'application/json',
-  },
-  body:JSON.stringify({formData})
-  }
-  const data= await fetch(reporterUrl,options)
-  const responseData=await data.json()
-  console.log(responseData)
-  setFormData("kkkkk")
+
+const onhandleChangeReporter=(e)=>{
+  const{name,value,type,files}=e.target
+  setFormData({...formData,
+    [name]:type==="file"?files[0]:value
+  })
+}
+
+const validate=()=>{
+  const newErrors={}
+
+  if(!formData.name) newErrors.name="Name is Required"
+  if(!formData.DOB) newErrors.DOB="Date Of Birth is Required"
+  if(!formData.gender) newErrors.gender="Please select Gender"
+  if(!formData.phone) newErrors.phone="Please Enter a Phone Number"
+  if(!formData.email) newErrors.phone="Gmail is Required"
+  if(!formData.address) newErrors.address="Address is Required"
+  
+  if(!formData.media) newErrors.media="Please Upload a Photo"
+  if(!formData.designation) newErrors.designation="Designation is Required"
+  if(!formData.pincode) newErrors.pincode="Pincode is Required"
+  if(!formData.experience) newErrors.experience="Enter 0 if you have no experience"
+  setErrors(newErrors)
+  return Object.keys(newErrors).length===0
+}
+
+
+
+
+const submitReporterForm= async (event)=>{
+   event.preventDefault()
+   if (validate()){
+       console.log("Form submitted:", formData);
+       try{
+        const formDataToSend = new FormData();
+
+    
+    Object.keys(formData).forEach((key) => {
+      if (key === "DOB") {
+        formDataToSend.append(key, formData.DOB ? formData.DOB.toISOString() : "");
+      } else {
+        formDataToSend.append(key, formData[key]);
+      }
+    });
+        const reporterUrl="http://34.100.231.173:3000/api/v1/reporter/application"
+        const options={method:"POST",
+        
+        body:formDataToSend
+        }
+        const data= await fetch(reporterUrl,options)
+        const responseData=await data.json()
+        console.log(responseData)
+        setFormData(initialFormdata)
+        setErrors({})
+        setIsFormSubmitted(true)
+
+      setTimeout(() => {
+      setIsFormSubmitted(false);
+      setIsReporterModal(false)
+    }, 3000);
+       }catch(e){
+        console.error("Error Submitting form",e)
+       }
+   }else{
+      console.log("Validation errors:", errors);
+   }
+
+
+  
+  
+  
 }
 
 
@@ -35,18 +99,22 @@ const submitReporterForm=async (event)=>{
     className="modal-content"
     overlayClassName="modal-overlay"
     >
+      {isSubmitted? <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h2>Reporter Form is Submitted!</h2>
+    </div>:
       
-      
-      <form className="rti-reporter-form-container" onSubmit={submitReporterForm}>
+      <form  encType="multipart/form-data" className="rti-reporter-form-container" onSubmit={submitReporterForm}>
         <h3 className="rti-reporter-heading">RTI REPORTER FORM</h3>
        <label className="rti-reporter-label-element">Name:</label>
-        <input name="name" className="rti-reporter-input-element" type="text"  placeholder="John Doe"/>
+        <input value={formData.name} name="name" className="rti-reporter-input-element" type="text"  placeholder="John Doe" onChange={onhandleChangeReporter}/>
+        {errors.name && <span style={{ color: "red" }}>{errors.name}</span>}
+         
          <label className="rti-reporter-label-element">Select Your DOB:</label>
           {/* <input className="rti-reporter-input-element" type="date"  placeholder="DD-MM-YYYY"/> */}
                <DatePicker
                     id="dob"
-                    selected={reporterdob}
-                    onChange={(date) => setReporterDob(date)}
+                    selected={formData.DOB}
+                    onChange={(date) =>setFormData({...formData,DOB:date})}
                     placeholderText="15/08/1947"
                     dateFormat="dd/MM/yyyy"
                     className="rti-reporter-input-element"
@@ -54,35 +122,56 @@ const submitReporterForm=async (event)=>{
                     showYearDropdown
                     scrollableYearDropdown
                   />
+        {errors.DOB&& <span style={{ color: "red" }}>{errors.DOB}</span>}
 
              <label className="rti-reporter-label-element">Select Your Gender:</label>
 
-    <select  name="gender" className="rti-reporter-input-element" placeholder="Male/Female/Other">
+    <select value={formData.gender}  name="gender" className="rti-reporter-input-element" placeholder="Male/Female/Other" onChange={onhandleChangeReporter}>
         <option value="">Select gender</option>
         <option value="male">Male</option>
         <option value="female">Female</option>
         <option value="other">Other</option>
   </select>
+          {errors.gender && <span style={{ color: "red" }}>{errors.gender}</span>}
+
    <label className="rti-reporter-label-element">Enter Your Phone Number:</label>
-     <input name="number"  className="rti-reporter-input-element" type="tel"  placeholder="+91 98765 43210"/>
+     <input value={formData.phone} name="phone"  className="rti-reporter-input-element" type="tel"  placeholder="+91 98765 43210" onChange={onhandleChangeReporter}/>
+      {errors.phone && <span style={{ color: "red" }}>{errors.phone}</span>}
+
              <label className="rti-reporter-label-element">Enter Your Gmail:</label>
-<input name="email" className="rti-reporter-input-element" type="email" placeholder="example@mail.com"/>
+<input  value={formData.email} name="email" className="rti-reporter-input-element" type="email" placeholder="example@mail.com" onChange={onhandleChangeReporter}/>
+            {errors.email && <span style={{ color: "red" }}>{errors.email}</span>}
+
+    
     <label className="rti-reporter-label-element">Enter Your Address:</label>
-<input name="address"  className="rti-reporter-input-element" type="text"  placeholder="123 Main St, City" />
+<input value={formData.address} name="address"  className="rti-reporter-input-element" type="text"  placeholder="123 Main St, City"  onChange={onhandleChangeReporter}/>
+            {errors.address && <span style={{ color: "red" }}>{errors.address}</span>}
+
      <label className="rti-reporter-label-element">Enter Your Adhaar Number:</label>
-      <input name="adhaar"  className="rti-reporter-input-element" type="text"  placeholder="1234 5678 9012"  maxLength="12"/>
+      <input value={formData.aadhar} name="aadhar"  className="rti-reporter-input-element" type="text"  placeholder="1234 5678 9012"  maxLength="12" onChange={onhandleChangeReporter}/>
+             {errors.aadhar && <span style={{ color: "red" }}>{errors.aadhar}</span>}
+
      <label className="rti-reporter-label-element">Upload Your Photo:</label>
-    <input name="image"  className="rti-reporter-input-element" type="file"  accept="image/*"/>
+    <input name="media"  className="rti-reporter-input-element" type="file"  accept="image/*" onChange={onhandleChangeReporter}/>
+            {errors.media && <span style={{ color: "red" }}>{errors.media}</span>}
+
     <label className="rti-reporter-label-element">Designation:</label>
-    <input name="dsignation" className="rti-reporter-input-element" type="text"  placeholder="Designation Eg... Junior Reporter,Reporter,Senior Reporter" />
-     <label className="rti-reporter-label-element">Enter Your Pincode:</label>
-    <input name="pincode"  className="rti-reporter-input-element" type="text"  placeholder="560001" />
+    <input value={formData.designation}  name="designation" className="rti-reporter-input-element" type="text"  placeholder="Designation Eg... Junior Reporter,Reporter,Senior Reporter"  onChange={onhandleChangeReporter}/>
+             {errors.designation && <span style={{ color: "red" }}>{errors.designation}</span>}
+
+      <label className="rti-reporter-label-element">Enter Your Pincode:</label>
+    <input value={formData.pincode}  name="pincode"  className="rti-reporter-input-element" type="text"  placeholder="560001" onChange={onhandleChangeReporter}/>
+             {errors.pincode && <span style={{ color: "red" }}>{errors.pincode}</span>}
+
      <label className="rti-reporter-label-element">Experience:</label>
-    <input name="experience" className="rti-reporter-input-element" type="text"  placeholder="Previous Experience If any like 3 years" />
-<button onClick={()=>setIsReporterModal(false)} type="submit" className="rti-reporter-submit-button">JOIN AS REPORTER</button>
+    <input value={formData.experience}  name="experience" className="rti-reporter-input-element" type="text"  placeholder="Previous Experience If any like 3 years"  onChange={onhandleChangeReporter}/>
+         {errors.experience && <span style={{ color: "red" }}>{errors.experience}</span>}
 
+ <button  type="submit" className="rti-reporter-submit-button">JOIN AS REPORTER</button>
 
-    </form> </Modal> 
+{/* onClick={()=>setIsReporterModal(false)} */}
+    </form> }
+    </Modal> 
 }
 
 export default RTIReporterForm
