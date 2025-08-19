746a6f937d101af541eeded431230bfa424f3584
yadaramsrikanth <yadaramsrikanth2000@gmail.com>
2025-08-19 17:33:08 +0530

    header-position-fixed

--- src/components/Legal/index.js
+++ src/components/Legal/index.js
@@ -3,16 +3,19 @@ import "./index.css"
 
 const LegalInfo=()=>{
     return <div className="legal-Information-conatiner">
-    
-<p>Legal Information</p>
+    <div className="legal-content-inside-container">
+<h2>RTI Express Legal Information</h2>
+
 <p>RNI:APTEL/2018/76797</p>
 <p>RNI:APTEL/2019/15838</p>
-<p> <span>Certificate of incorporation:</span>U64200AP2021PTC118146</p>
+<p> Certificate of incorporation:
+U64200AP2021PTC118146</p>
 <p>MSME:UDYAM-AP-250038710</p>
 <p>Register of Firms No:(43 Of 2018)</p>
 <p>Labour Reg No:AP-25-18-006-03671823</p>
 <p>Postal licenced:HDP/010/2024-2026</p>
 <p>GST No:37ABJPF4955F1ZK</p>
+</div>
 </div>
 }
 
